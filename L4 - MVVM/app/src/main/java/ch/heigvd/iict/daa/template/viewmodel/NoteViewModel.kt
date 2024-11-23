9c5114bf053e820dde9f53e9b11fd7d0876d0114
Dahïr <better_blood@hotmail.com>
2024-11-23 15:32:09 +0100

    log

--- L4 - MVVM/app/src/main/java/ch/heigvd/iict/daa/template/viewmodel/NoteViewModel.kt
+++ L4 - MVVM/app/src/main/java/ch/heigvd/iict/daa/template/viewmodel/NoteViewModel.kt
@@ -1,5 +1,6 @@
 package ch.heigvd.iict.daa.template.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.*
 import ch.heigvd.iict.daa.template.entities.Note
 import ch.heigvd.iict.daa.template.entities.NoteAndSchedule
@@ -23,7 +24,11 @@ class NoteViewModel(private val repository: NoteRepository) : ViewModel() {
                 SortType.BY_SCHEDULE -> notes.sortedBy { it.schedule?.date }
                 SortType.NONE -> notes
             }
+            Log.d("DEBUG", "init notes : " + notes)
         }
+        Log.d("DEBUG", "init _sortedNotes : " + _sortedNotes.value)
+        Log.d("DEBUG", "init allNotes : " + allNotes)
+        Log.d("DEBUG", "init allNotes.value : " + allNotes.value)
     }
 
     fun generateANote() {
@@ -33,11 +38,25 @@ class NoteViewModel(private val repository: NoteRepository) : ViewModel() {
     }
 
     fun sortByCreationDate() {
+        Log.d("DEBUG", "BY_DATE")
+        Log.d("DEBUG", _sortedNotes.toString())
+        Log.d("DEBUG", _sortedNotes.value?.size.toString())
+        Log.d("DEBUG", _sortedNotes.value.toString())
+        Log.d("DEBUG", "sortByCreationDate allNotes : " + allNotes)
+        Log.d("DEBUG", "sortByCreationDate allNotes.value : " + allNotes.value)
+        Log.d("DEBUG", "sortByCreationDate repository.allNotes.value : " + repository.allNotes.value)
+
+
         currentSortType = SortType.BY_DATE
         _sortedNotes.value = _sortedNotes.value?.sortedBy { it.note.creationDate }
     }
 
     fun sortBySchedule() {
+        Log.d("DEBUG", "BY_SCHEDULE")
+        Log.d("DEBUG", _sortedNotes.toString())
+        Log.d("DEBUG", _sortedNotes.value?.size.toString())
+        Log.d("DEBUG", _sortedNotes.value.toString())
+
         currentSortType = SortType.BY_SCHEDULE
         _sortedNotes.value = _sortedNotes.value?.sortedBy { it.schedule?.date }
     }
